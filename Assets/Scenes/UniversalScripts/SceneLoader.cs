9ef2b391baf92a80351c8a703436d46339db124d
Боевой робот «Иосиф Сталин» <joseph_stalin@gmail.com>
2026-09-24 06:51:26 +0300

    Added folder UniversalScripts containing universal scripts for Test and Game scenes

---
+++ Assets/Scenes/UniversalScripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private int sceneIndex = 1;
+
+    void LoadSceneIndex()
+    {
+        if (sceneIndex < 0 || sceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.LogError($"Указан несуществующий индекс сцены ({sceneIndex})");
+            return;
+        }
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
